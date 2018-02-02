3a22fbf6766707a171e963be70dc7d6074237de1
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-02-02 16:04:44 +0530

    misc

--- src/main/scala/tmt/tools/ScriptCompiler.scala
+++ src/main/scala/tmt/tools/ScriptCompiler.scala
@@ -7,5 +7,5 @@ import tmt.sequencer.ScriptFactory
 object ScriptCompiler extends App {
   println(args.toList)
   println(System.getProperty("user.dir"))
-  ScriptFactory.fromFile(new File(args(0)))
+  ScriptFactory.fromFile(new File("scripts/simple.ss"))
 }
