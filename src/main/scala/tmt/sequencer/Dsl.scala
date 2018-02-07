66516335ea3db23227a93d2df869d5961ac211b2
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-02-07 09:37:34 +0530

    separate out ControlDsl

--- src/main/scala/tmt/sequencer/Dsl.scala
+++ src/main/scala/tmt/sequencer/Dsl.scala
@@ -1,13 +1,8 @@
 package tmt.sequencer
 
-import tmt.sequencer.FutureExt.RichFuture
-import tmt.services.CommandResponse
-
-import scala.concurrent.Future
 import scala.language.implicitConversions
-import scala.concurrent.ExecutionContext.Implicits.global
 
-object Dsl {
+object Dsl extends ControlDsl {
   private[tmt] val wiring = new Wiring
   private[tmt] def init(): Unit = {
     //touch system so that main does not exit
@@ -16,20 +11,6 @@ object Dsl {
 
   lazy val cs: CommandService = wiring.commandService
   lazy val engine: Engine = wiring.engine
-
-  implicit def toFuture(x: => CommandResponse): Future[CommandResponse] = Future(x)
-
-  def forEach(f: Command => Unit): Unit = {
-    loop(f(wiring.engine.pullNext()))
-  }
-
-  def loop(block: => Unit): Unit = Future {
-    while (true) {
-      block
-    }
-  }
-
-  def par[T](fs: Future[T]*): Seq[T] = Future.sequence(fs.toList).await
 
   val Command = tmt.services.Command
   type Command = tmt.services.Command
--- src/main/scala/tmt/sequencer/RemoteRepl.scala
+++ src/main/scala/tmt/sequencer/RemoteRepl.scala
@@ -4,17 +4,17 @@ import ammonite.sshd._
 import org.apache.sshd.server.auth.password.AcceptAllPasswordAuthenticator
 
 object RemoteRepl {
-  lazy val server = new SshdRepl(
-    SshServerConfig(
-      address = "localhost", // or "0.0.0.0" for public-facing shells
-      port = 22222, // Any available port
-      passwordAuthenticator = Some(AcceptAllPasswordAuthenticator.INSTANCE) // or publicKeyAuthenticator
-    ),
-    predef =
-      """
-        |import tmt.services.Command
-        |import tmt.sequencer.Dsl._
-        |repl.compiler.settings.Ydelambdafy.value = "inline"
-      """.stripMargin
+  private def sshServerConfig = SshServerConfig(
+    address = "localhost", // or "0.0.0.0" for public-facing shells
+    port = 22222, // Any available port
+    passwordAuthenticator = Some(AcceptAllPasswordAuthenticator.INSTANCE) // or publicKeyAuthenticator
   )
+
+  private def initialCommands: String =
+    """import tmt.services.Command
+       import tmt.sequencer.Dsl._
+       repl.compiler.settings.Ydelambdafy.value = "inline"
+      """.stripMargin
+
+  def server = new SshdRepl(sshServerConfig, initialCommands)
 }
