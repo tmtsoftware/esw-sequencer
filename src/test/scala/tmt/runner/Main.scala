c09594b1719e9644d4185c3541882c107335e7f1
Bharat Singh <bharats@thoughtworks.com>
2018-03-28 13:41:46 +0530

    misc

--- src/test/scala/tmt/runner/Main.scala
+++ src/test/scala/tmt/runner/Main.scala
@@ -4,7 +4,7 @@ import tmt.sequencer.ScriptRunner
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val scriptFile = args.headOption.getOrElse("src/test/scala/scripts/iris_sequencer_parallel.sc")
+    val scriptFile = args.headOption.getOrElse("src/test/scala/scripts/IrisParallel.sc")
     ScriptRunner.run(scriptFile, isProd = false)
   }
 }
