2120d300a40b9f07c97b157fdd04812c90c591fe
Poorva Gokhale <13188905+Poorva17@users.noreply.github.com>
2018-04-12 22:30:44 +0530

    Allow cors for RPC server
    Co-authored-by: Mushtaq Ahmed <mushtaq.a@gmail.com>
    Co-authored-by: Bharat Singh <10626464+Bharat1103@users.noreply.github.com>

--- src/main/scala/tmt/sequencer/rpc/server/RpcServer.scala
+++ src/main/scala/tmt/sequencer/rpc/server/RpcServer.scala
@@ -2,11 +2,14 @@ package tmt.sequencer.rpc.server
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.headers.{HttpOrigin, HttpOriginRange}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.RouteResult._
 import akka.stream.ActorMaterializer
 import covenant.http.AkkaHttpRoute
 import covenant.http.ByteBufferImplicits._
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
+import ch.megard.akka.http.cors.scaladsl.settings.CorsSettings
 
 import scala.concurrent.Future
 
@@ -14,7 +17,10 @@ class RpcServer(rpcConfigs: RpcConfigs, routes: Routes)(implicit system: ActorSy
   private implicit val materializer: ActorMaterializer = ActorMaterializer()
   import materializer.executionContext
 
-  private val route = {
+  val settings =
+    CorsSettings.defaultSettings.withAllowGenericHttpRequests(true)
+
+  private val route = cors(settings) {
     pathPrefix("something-later") {
       complete("test-done")
     } ~
