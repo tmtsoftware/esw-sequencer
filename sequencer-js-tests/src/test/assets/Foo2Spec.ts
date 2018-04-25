4da4d901af796aa0293914119fe9f699dd915eff
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-04-25 09:07:50 +0530

    this works.

--- sequencer-js-tests/src/test/assets/Foo2Spec.ts
+++ sequencer-js-tests/src/test/assets/Foo2Spec.ts
@@ -3,7 +3,7 @@ import mysequencer = require("mysequencer");
 describe("Foo2", () => {
   it("say hello111111", () => {
       console.log("aaaaaa");
-      console.log(mysequencer.default.AA.dd());
+      console.log(mysequencer.AA.dd());
 //     // assert.equal(foo.hello("world"), "hello world");
   });
 });
