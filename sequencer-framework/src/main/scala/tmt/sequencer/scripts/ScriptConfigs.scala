45ac163423924a72d50961503ffac3d80263172b
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-05-04 14:01:11 +0530

    Remove default settings Allow loading script class using only sequencer Id

--- sequencer-framework/src/main/scala/tmt/sequencer/scripts/ScriptConfigs.scala
+++ sequencer-framework/src/main/scala/tmt/sequencer/scripts/ScriptConfigs.scala
@@ -1,26 +1,21 @@
 package tmt.sequencer.scripts
 
 import akka.actor.ActorSystem
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigFactory}
 
 import scala.util.Try
 
-class ScriptConfigs(sClass: Option[String], oMode: Option[String])(implicit actorSystem: ActorSystem) {
+class ScriptConfigs(sequencerId: String, oMode: String)(implicit actorSystem: ActorSystem) {
 
-  private val config: Config     = actorSystem.settings.config.getConfig("scripts")
-  private val defaultSequencerId = config.getString("defaultSequencerId")
-
-  val observingMode: String = oMode.getOrElse(config.getString("defaultObservingMode"))
+  private lazy val config: Config = ConfigFactory
+    .parseResources(s"$sequencerId.conf")
+    .withFallback(actorSystem.settings.config)
 
   lazy val scriptClass: String =
     Try(
-      sClass.getOrElse(
-        config
-          .getConfig(s"$defaultSequencerId.$observingMode")
-          .getString("scriptClass")
-      )
+      config.getString(s"scripts.$sequencerId.$oMode.scriptClass")
     ).toOption
       .getOrElse(
-        throw new RuntimeException("Please provide script class either through command line or configuration settings")
+        throw new RuntimeException(s"Please provide script class for ${sequencerId} in configuration settings")
       )
 }
