446a581face8f5d5626c497ad9811845416aa2f4
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-05-30 16:18:25 +0530

    Remove logic to clone script repository Remove main method from framework

--- sequencer-framework/src/main/scala/tmt/sequencer/SequencerApp.scala
+++ sequencer-framework/src/main/scala/tmt/sequencer/SequencerApp.scala
@@ -1,19 +1,9 @@
 package tmt.sequencer
 
 object SequencerApp {
-  def main(args: Array[String]): Unit = {
-    val (sequencerId, observingMode, port, isProd) = args match {
-      case Array(sId, oMode, p, isP) => (sId, oMode, Some(p.toInt), isP.toBoolean)
-      case Array(sId, oMode, p)      => (sId, oMode, Some(p.toInt), true)
-      case Array(sId, oMode)         => (sId, oMode, None, true)
-      case _                         => throw new RuntimeException("please provide both sequencerId and observationMode parameters")
-    }
-
-    val wiring = new Wiring(sequencerId, observingMode, port, isProd)
+  def run(sequencerId: String, observingMode: String, port: Option[Int]): Unit = {
+    val wiring = new Wiring(sequencerId, observingMode, port)
     import wiring._
-    if (isProd) {
-      scriptRepo.cloneRepo()
-    }
     engine.start(sequencer, script)
     rpcServer.start()
 //    rpcServer2.start()
--- sequencer-framework/src/main/scala/tmt/sequencer/Wiring.scala
+++ sequencer-framework/src/main/scala/tmt/sequencer/Wiring.scala
@@ -7,7 +7,6 @@ import akka.stream.{ActorMaterializer, Materializer}
 import akka.util.Timeout
 import ammonite.ops.{Path, RelPath}
 import tmt.sequencer.api.{SequenceEditor, SequenceFeeder}
-import tmt.sequencer.git.{ScriptConfigs, ScriptRepo}
 import tmt.sequencer.dsl.{CswServices, Script}
 import tmt.sequencer.gateway.LocationService
 import tmt.sequencer.messages.{SequencerMsg, SupervisorMsg}
@@ -15,16 +14,14 @@ import tmt.sequencer.rpc.server._
 
 import scala.concurrent.duration.DurationDouble
 
-class Wiring(sequencerId: String, observingMode: String, port: Option[Int], isProd: Boolean) {
+class Wiring(sequencerId: String, observingMode: String, port: Option[Int]) {
   implicit lazy val timeout: Timeout           = Timeout(5.seconds)
   lazy implicit val system: ActorSystem        = ActorSystem("test")
   lazy implicit val materializer: Materializer = ActorMaterializer()
   import system.dispatcher
 
   lazy val scriptConfigs = new ScriptConfigs(system)
-  lazy val repoDir: Path = if (isProd) Path(scriptConfigs.cloneDir) else ammonite.ops.pwd
-  lazy val scriptRepo    = new ScriptRepo(scriptConfigs, locationService)
-  lazy val path: Path    = repoDir / RelPath(scriptConfigs.scriptFactoryPath)
+  lazy val path: Path    = ammonite.ops.pwd / RelPath(scriptConfigs.scriptFactoryPath)
 
   lazy val sequencerRef: ActorRef[SequencerMsg] = system.spawn(SequencerBehaviour.behavior, "sequencer")
   lazy val sequencer                            = new Sequencer(sequencerRef, system)
